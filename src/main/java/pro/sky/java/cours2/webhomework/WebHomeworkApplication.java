5f3a54b77b1e990bc0accd36fbef6d2050618a10
alekdersan <repka1795@gmail.com>
2022-02-23 11:16:00 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/cours2/webhomework/WebHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.cours2.webhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHomeworkApplication.class, args);
+    }
+
+}
