23c19298841363ec53356d26021bb735417b3c29
alekdersan <repka1795@gmail.com>
2022-04-20 23:40:56 +0300

    My first commit before hw 2.14

---
+++ src/test/java/pro/sky/java/cours2/webhomework/service/impl/EmployeeServiceImplTest.java
@@ -0,0 +1,24 @@
+package pro.sky.java.cours2.webhomework.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceImplTest {
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void removeEmployee() {
+    }
+
+    @Test
+    void findEmployee() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+}
